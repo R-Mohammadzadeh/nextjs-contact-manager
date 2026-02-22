feb17effe4bb78623cd2722dedb67d1724dc7180
R-Mohammadzadeh <reza203393@yahoo.de>
2026-02-22 18:23:59 +0100

    loadin and progresbar
    Signed-off-by: R-Mohammadzadeh <reza203393@yahoo.de>

--- components/loader/Loader.jsx
+++ components/loader/Loader.jsx
@@ -1,10 +1,10 @@
-import styles from './Loader.module.css'
+// import styles from './Loader.module.css'
 
-export default function Loading() {
-    return (
-        <div className={styles.container}>
-            <div className={styles.spinner}></div>
-            <p>LOADING...</p>
-        </div>
-    );
-}
+// export default function Loading() {
+//     return (
+//         <div className={styles.container}>
+//             <div className={styles.spinner}></div>
+//             <p>LOADING...</p>
+//         </div>
+//     );
+// }
--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,36 @@
 import MyNavbar from "@/components/navbar/navbar";
 import "@/styles/globals.css";
+import 'nprogress/nprogress.css';
+import "@/styles/globals.css";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import Loader from '../components/loader/Loader.jsx'
+import { useEffect } from "react";
+
+
+
+NProgress.configure({ showSpinner: false, speed: 500, minimum: 0.3 });
+
 export default function App({ Component, pageProps }) {
-
-const router = useRouter()
-
-const [loading , setLoading] = useState(false)
+const router = useRouter();
 
 useEffect(() => {
-  // Funktion, die beim Seitenwechsel ausgeführt wird
-  const handelStart = () => setLoading(true) ;
-  // Funktion, die nach Abschluss des Seitenwechsels ausgeführt werden soll
-  const handleComplete = () => setLoading(false)
-router.events.on('hashChangeStart' , handelStart)
-router.events.on('hashChangeComplete' ,handleComplete)
-router.events.on('routeChangeError' , handleComplete)
-} , [router])
+    const handleStart = () => NProgress.start();
+    const handleStop = () => NProgress.done();
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
+
 
   return (
     <>
     <MyNavbar />
-    {loading && <Loader />}
     <Component {...pageProps} />
     </>
   )
