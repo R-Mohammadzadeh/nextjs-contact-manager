8a75a8276ff546c10403020c5ab251d93241ced6
R-Mohammadzadeh <reza203393@yahoo.de>
2026-02-22 21:46:49 +0100

    stoped loading
    Signed-off-by: R-Mohammadzadeh <reza203393@yahoo.de>

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,31 @@
 import MyNavbar from "@/components/navbar/navbar";
-import "@/styles/globals.css";
-import 'nprogress/nprogress.css';
-import "@/styles/globals.css";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+// import "@/styles/globals.css";
+// import 'nprogress/nprogress.css';
+// import "@/styles/globals.css";
+// import { useRouter } from "next/router";
+// import { useEffect } from "react";
 
 
 
-NProgress.configure({ showSpinner: false, speed: 500, minimum: 0.3 });
+// NProgress.configure({ showSpinner: false, speed: 500, minimum: 0.3 });
 
 export default function App({ Component, pageProps }) {
-const router = useRouter();
+// const router = useRouter();
 
-useEffect(() => {
-    const handleStart = () => NProgress.start();
-    const handleStop = () => NProgress.done();
+// useEffect(() => {
+//     const handleStart = () => NProgress.start();
+//     const handleStop = () => NProgress.done();
 
-    router.events.on('routeChangeStart', handleStart);
-    router.events.on('routeChangeComplete', handleStop);
-    router.events.on('routeChangeError', handleStop);
+//     router.events.on('routeChangeStart', handleStart);
+//     router.events.on('routeChangeComplete', handleStop);
+//     router.events.on('routeChangeError', handleStop);
 
-    return () => {
-      router.events.off('routeChangeStart', handleStart);
-      router.events.off('routeChangeComplete', handleStop);
-      router.events.off('routeChangeError', handleStop);
-    };
-  }, [router]);
+//     return () => {
+//       router.events.off('routeChangeStart', handleStart);
+//       router.events.off('routeChangeComplete', handleStop);
+//       router.events.off('routeChangeError', handleStop);
+//     };
+//   }, [router]);
 
 
   return (
