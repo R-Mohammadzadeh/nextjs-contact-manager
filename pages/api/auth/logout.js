b758c10a0226b902c1c3864831d30530e9e230ac
Reza Mohammadzadeh <reza203398@yahoo.de>
2026-03-27 19:58:10 +0100

    API
    Signed-off-by: Reza Mohammadzadeh <reza203398@yahoo.de>

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -5,16 +5,17 @@ export default function handler(req, res) {
     return res.status(405).json({ message: "Method not allowed" });
   }
 
-//  Clear the token cookie by setting it to an empty value and expiring it immediately
-  const serialized = serialize("token", "", {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-   expires: new Date(0), // Set the cookie to expire immediately
-   path: "/", // Ensure the cookie is cleared for the entire site
-  });
-
-  res.setHeader("Set-Cookie", serialized);
-
+  res.setHeader(
+    "Set-Cookie",
+    serialize("token", "", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict", 
+      expires: new Date(0), // Set the cookie to expire immediately
+      path: "/",
+      domain: "localhost", 
+  
+    })
+  );
   return res.status(200).json({ message: "Logged out successfully" });
 }
